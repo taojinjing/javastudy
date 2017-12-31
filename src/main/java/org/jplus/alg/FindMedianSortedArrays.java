2832300b598f7b07d23de78ebd3b715d2b2785a9
ftao <frank.tao@flatironssolutions.com>
2017-12-31 11:41:42 +0800

    init version of findMedianSortedArrays

--- src/main/java/org/jplus/alg/FindMedianSortedArrays.java
+++ src/main/java/org/jplus/alg/FindMedianSortedArrays.java
@@ -4,44 +4,49 @@ public class FindMedianSortedArrays {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int i = -1;
         int j = -1;
-        int mediaIndex = (nums1.length + nums2.length) / 2;
+        int totalLength = nums1.length + nums2.length;
         int indexOn = 0;
-        while (i + j + 1 < mediaIndex) {
-            if (i + 1 == nums1.length) {
-                j = mediaIndex - i - 1;
-                break;
-            } else if (j + 1 == nums2.length) {
-                i = mediaIndex - j - 1;
-                break;
-            }
-            if (indexOn == 0) {
-                if (nums1[i + 1] > nums2[j + 1]) {
-                    j++;
-                    indexOn = 2;
-                } else {
-                    i++;
-                    indexOn = 1;
-                }
-            } else if (indexOn == 1) {
-                if (nums2[j + 1] > nums1[i + 1]) {
-                    i++;
-                } else {
-                    j++;
-                    indexOn = 2;
-                }
-            } else if (indexOn == 2) {
-                if (nums2[j + 1] > nums1[i + 1]) {
+        int beforeIndex = 0;
+        double lastNumber = 0;
+        while (true) {
+            if (i + 1 == nums1.length && j + 1 < nums2.length) {
+                j++;
+                indexOn = 2;
+            } else if (j + 1 == nums2.length && i + 1 < nums1.length) {
+                i++;
+                indexOn = 1;
+            } else if (i + 1 < nums1.length && j + 1 < nums2.length) {
+                if (nums1[i + 1] < nums2[j + 1]) {
                     i++;
                     indexOn = 1;
                 } else {
                     j++;
+                    indexOn = 2;
                 }
             }
-        }
-        if (indexOn == 1) {
-            return nums1[i];
-        } else {
-            return nums2[j];
+            if ((totalLength % 2) == 0) {
+                if (beforeIndex == (totalLength - 2) / 2 + 1) {
+                    if (indexOn == 1) {
+                        return (lastNumber + (double) nums1[i]) / 2;
+                    } else {
+                        return (lastNumber + (double) nums2[j]) / 2;
+                    }
+                }
+                if (indexOn == 1) {
+                    lastNumber = nums1[i];
+                } else {
+                    lastNumber = nums2[j];
+                }
+            } else {
+                if (beforeIndex == (totalLength - 1) / 2) {
+                    if (indexOn == 1) {
+                        return nums1[i];
+                    } else {
+                        return nums2[j];
+                    }
+                }
+            }
+            beforeIndex++;
         }
     }
 }
